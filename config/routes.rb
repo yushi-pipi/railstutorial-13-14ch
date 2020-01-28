0c819067924865fd770423f5964cb8df85fcd516
池田侑史 <ikedayuushi@ikedayuushinoMacBook-Air.local>
2020-01-28 17:45:04 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'password_reset/new'
+
+  get 'password_reset/edit'
+
+  get "sessions/new"
+
+  root "static_pages#home"
+  get "/help", to: "static_pages#help"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
+  get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  post "/signup", to: "users#create"
+  resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+end
---
+++ test/controllers/password_reset_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class PasswordResetControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    #get password_reset_new_url
+    #assert_response :success
+  end
+
+  test "should get edit" do
+    #get password_reset_edit_url
+    #assert_response :success
+  end
+end
